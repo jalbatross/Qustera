03bf9514952210763007d810175d812d1fa4883f
jalbatross <ih8u2o@gmail.com>
2016-06-25 11:26:31 -0700

    Updated entry page

--- Web/entry/js/index.js
+++ Web/entry/js/index.js
@@ -1,6 +1,8 @@
 /*jslint browser: true, indent: 4 */
 /*global $, jQuery, alert*/
 
+//TODO: Eventually move auxillary functions before document ready function.
+
 $(document).ready(function () {
     "use strict";
     
@@ -8,7 +10,7 @@ $(document).ready(function () {
     var currentSlide = 0;
     var slideWidth = $('.slideContainer').css('width');
     var i, k = 0;
-    
+    var validTitle, validQuest;
     
     //fix z-indices of slides
     function setSlideZ() {
@@ -19,13 +21,15 @@ $(document).ready(function () {
         }
         
     };
-    setSlideZ();
     
     //set enabled/disabled states for buttons
     function setButtonState() {
         switch (currentSlide) {
             case 0: 
                 $('.left > .btn').prop('disabled', true);
+                if(!validTitle) {
+                    $('.right > .btn').prop('disabled', true);
+                }
                 break;
             case numSlides - 1: 
                 $('.right > .btn').prop('disabled', true);
@@ -33,6 +37,7 @@ $(document).ready(function () {
             default:
                 $('.navButton > .btn').prop('disabled', false);
         }
+            
     };
 
     function slideRight() {
@@ -48,20 +53,26 @@ $(document).ready(function () {
         currentSlide--;
         setButtonState();
     }
-        
-    setButtonState();
     
+
     //TODO: Add swipe functionality
-    
     //navButton click slide
     $('.navButton').click(function () {
         if ($(this).hasClass('right')) {
+            
+            if(currentSlide === 0) {
+                submitQuestName();
+                return;
+            }
+            
             slideRight();
+            
         }
         else if ($(this).hasClass('left')) {
             slideLeft();
         }
     });
+
     
     //removes HTML tags from strings
     function stripHTML(dirtyString) {
@@ -72,26 +83,87 @@ $(document).ready(function () {
     }
     
     //Get quest name
+    //TODO: Disable scrolling to second page and further IF this entry
+    //field is blank, or has not been submitted before.
+    //TODO: Prevent quest titles that are too short (maybe one char long?)
+    //TODO: Filter for nonsensical titles (maybe use dictionary)
     $("#slide1 > form").on('submit', function(e) {
         e.preventDefault();
-        var formString = stripHTML($("#slide1 >form > fieldset > input").val());
-        $("#slide2").html(formString + "<br> that sounds like fun!");
-        slideRight();
-        return false;
+        if (!preventBadInput){
+            submitQuestName();
+            console.log("prevent");
+        }
+        else {
+            return false;
+        }
     });
     
-    function preventLongInput() {
-      var maxTitleLength = 16; //Need to globally define this somewhere else
+    function submitQuestName() {
+        var formString = stripHTML($("#slide1 >form > fieldset > input").val());
+        $("#slide2 > fieldset > h").html(formString + "<br><small class= " + '"text-muted"' + "> that sounds like fun!</small>");
+        slideRight();
+        validTitle = true;
+    }
+    
+    
+    
+    //limit for quest title
+    function preventBadInput() {
+        var maxTitleLength = 32; //Need to get this from html
+        var minTitleLength = 2;
         var $title = $(this);
         var titleLength = $title.val().length;
         
-        if (titleLength >= maxTitleLength) {
+        if (titleLength >= maxTitleLength || titleLength < minTitleLength) {
             $title.addClass('invalid');
+            $('.right > .btn').prop('disabled', true);
+            return false;
         }
         else {
             $title.removeClass('invalid');
+            $('.right > .btn').prop('disabled', false);
+            return true;
         }
     }
-    $("#slide1>form>fieldset>input").on("input", preventLongInput);
+    $("#slide1>form>fieldset>input").on("input", preventBadInput);
+    
+    //prevents proceeding for step two
+    function questDescriptionProceed() {
+        var maxDescriptionLength = 512; // get from html
+        var minDescriptionLength = 2;
+        var $description = $(this);
+        var descriptionLength = $description.val().length;
+        
+        if (descriptionLength < minDescriptionLength || 
+            descriptionLength > maxDescriptionLength) {
+            $description.next().removeClass('btn-primary');
+            $description.next().addClass('btn-secondary disabled');
+        }
+        else {
+            $description.next().removeClass('btn-secondary disabled');
+            $description.next().addClass('btn-primary');
+        }
+    }
+    $("#slide2>fieldset>textarea").on("input", questDescriptionProceed);
+    
+    //submit quest description
+    $("#slide2>fieldset>button").on('click',function(e) {
+        e.preventDefault();
+        if ($("#slide2>fieldset>button").hasClass('disabled')) {
+            return false;
+        }
+        else {
+            if (!validQuest)
+                validQuest = true;
+            slideRight();
+        }
+    });
+    
+    function init() {
+        setSlideZ();
+        setButtonState();
+        validTitle = validQuest = false;
+    }
+    init();
     
 });
